77037ba1a83a2767a4734d4012638b96c7842445
Sebastian <jssossa2@misena.edu.co>
2023-03-17 23:48:23 -0500

    Actu 3 .java

---
+++ src/volumenPiscina.java
@@ -0,0 +1,25 @@
+//
+
+import java.util.Scanner;
+public class volumenPiscina {
+
+
+    public static void main(String[]args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese la altura de la piscina:");
+        int altura = sc.nextInt();
+
+        System.out.println("Ingrese el ancho de la piscina:");
+        int ancho = sc.nextInt();
+
+        System.out.println("Ingrese la largo de la piscina:");
+        int largo = sc.nextInt();
+
+        int volumen = altura * ancho * largo;
+
+        System.out.println("El volumen de la piscina es: " + volumen);
+    }
+
+}
